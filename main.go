070fe448be653ad48e4fb1831669420cd905a099
Dinesh Sharma <dineshsharma@Dineshs-Air.hsd1.ca.comcast.net>
2020-01-25 19:20:37 -0800

    minor change - added a request counter on 01/25/2020

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 // add it to develop branch
+//changed on 01/25/2020
 import ( "log"
 	"fmt"
 	"time"
 	"net/http"
 )
+var cnt int = 0
 
 func homePage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Homepage Endpoint hit at %v", time.Now())
@@ -12,6 +14,8 @@ func homePage(w http.ResponseWriter, r *http.Request){
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	cnt += 1
+	fmt.Println("Request counter at %v is %d ", time.Now(), cnt) 
 	log.Fatal(http.ListenAndServe(":8082", nil))
 }
 func main() {
